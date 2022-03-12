ac1ee20082d7fb33aa46fc813b720cb57c127d15
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-03-12 18:26:55 -0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SectionComponent } from './components/section/section.component';
+import { MetricsComponent } from './components/metrics/metrics.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SectionComponent,
+    MetricsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
