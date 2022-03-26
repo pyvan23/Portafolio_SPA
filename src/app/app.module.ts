0cd8c56b987e4d55a665b8efab8a27b89ac2af7a
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-03-26 17:23:49 -0300

    header and section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SectionComponent } from './components/section/section.component';
 import { MetricsComponent } from './components/metrics/metrics.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     SectionComponent,
     MetricsComponent,
     FooterComponent,
+    ProgressBarComponent,
     
   ],
   imports: [
