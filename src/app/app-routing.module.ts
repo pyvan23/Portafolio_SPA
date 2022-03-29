6498423026a129e573d99290bc4cc9850905c36e
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-03-29 07:27:47 -0300

    setup forms,login,routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import{EditComponent}from'./components/edit/edit.component'
 import { Router } from 'express';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { PorfolioComponent } from './components/porfolio/porfolio.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' },
+  { path: 'porfolio', component: PorfolioComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SectionComponent } from './components/section/section.component';
 import { MetricsComponent } from './components/metrics/metrics.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EditComponent } from './components/edit/edit.component';
+import { LoginComponent } from './components/login/login.component';
+import { PorfolioComponent } from './components/porfolio/porfolio.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     SectionComponent,
     MetricsComponent,
     FooterComponent,
+    EditComponent,
+    LoginComponent,
+    PorfolioComponent,
     
     
   ],
@@ -25,7 +30,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
